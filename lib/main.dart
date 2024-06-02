522199b016fa57fdd0c5b9fd06db73c087a6d62f
Claylton Santos <claylton.santos@dilettasolutions.com>
2024-06-02 11:53:38 -0300

    🌈 style(ios/home_page): Style ios page
    Create style ios page

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:imc/ui/android/my_material_app.dart';
 import 'package:imc/ui/ios/my_cupertino_app.dart';
 
-void main() => runApp(const MyMaterialApp());
-// void main() => runApp(const MyCupertinoApp());
+// void main() => runApp(const MyMaterialApp());
+void main() => runApp(const MyCupertinoApp());
--- lib/ui/ios/pages/home_page.dart
+++ lib/ui/ios/pages/home_page.dart
@@ -1,19 +1,52 @@
-
-
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-
-  const HomePage({ super.key });
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
-   @override
-   Widget build(BuildContext context) {
-       return Container();
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('Cálculo de IMC'),
+      ),
+      child: ListView(
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.all(20),
+            child: CupertinoTextField(
+              placeholder: 'Altura (cm)',
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(20),
+            child: CupertinoTextField(
+              placeholder: 'Peso (kg)',
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Você está fora de forma',
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: CupertinoButton.filled(
+              child: const Text('Calcular'),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
