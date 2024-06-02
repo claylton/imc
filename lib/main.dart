a3195521dd5934bfd3287aaaa6009fffacfe5d78
Claylton Santos <clayltonsp@hotmail.com>
2024-06-02 17:32:35 -0300

    ✨ feat(main): Change layout system
    Change layout system ios or android

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:imc/ui/android/my_material_app.dart';
 import 'package:imc/ui/ios/my_cupertino_app.dart';
+import 'dart:io' show Platform;
 
-// void main() => runApp(const MyMaterialApp());
-void main() => runApp(const MyCupertinoApp());
+
+void main() => runApp(Platform.isIOS ? const MyCupertinoApp() : const MyMaterialApp());
