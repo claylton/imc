0560fe8cac23a10c7cdd0cdd9a977bf33826b49e
Claylton Santos <clayltonsp@hotmail.com>
2024-06-20 10:56:43 -0300

    🦄 refactor: logic imc

--- lib/blocs/imc_bloc.dart
+++ lib/blocs/imc_bloc.dart
@@ -10,18 +10,20 @@ class ImcBloc {
     double height = double.parse(heightMaskTextInputFormatter.getMaskedText());
     double imc = weight / (height * height);
 
-    if (imc < 18.6) {
-      result = "Abaixo do peso (${imc.toStringAsPrecision(2)})";
-    } else if (imc >= 18.6 && imc < 24.9) {
-      result = "Peso ideal (${imc.toStringAsPrecision(2)})";
-    } else if (imc >= 24.9 && imc < 29.9) {
-      result = "Levemente acima do peso (${imc.toStringAsPrecision(2)})";
-    } else if (imc >= 29.9 && imc < 34.9) {
-      result = "Obesidade grau I (${imc.toStringAsPrecision(2)})";
-    } else if (imc >= 34.9 && imc < 39.9) {
-      result = "Obesidade grau II (${imc.toStringAsPrecision(2)})";
+    if (imc < 17) {
+      result = "Muito abaixo do peso (${imc.toStringAsPrecision(4)})";
+    } else if (imc >= 17 && imc < 18.49) {
+      result = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
+    } else if (imc >= 18.50 && imc < 24.99) {
+      result = "Peso ideal (${imc.toStringAsPrecision(4)})";
+    } else if (imc >= 25 && imc < 29.99) {
+      result = "Levemente acima do peso (${imc.toStringAsPrecision(4)})";
+    } else if (imc >= 30 && imc < 34.99) {
+      result = "Obesidade grau I (${imc.toStringAsPrecision(4)})";
+    } else if (imc >= 35 && imc < 39.99) {
+      result = "Obesidade grau II (${imc.toStringAsPrecision(4)})";
     } else {
-      result = "Obesidade grau III (${imc.toStringAsPrecision(2)})";
+      result = "Obesidade grau III (${imc.toStringAsPrecision(4)})";
     }
   }
   
